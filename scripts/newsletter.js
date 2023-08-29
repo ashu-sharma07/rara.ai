3555354426be674f0665b15b901319c3112f8442
Ashu Sharma <ashusharma0771@gmail.com>
2023-08-29 11:38:58 +0530

    newsletter add

---
+++ scripts/newsletter.js
@@ -0,0 +1,44 @@
+const emailInput = document.querySelector(".a1-newsletter input");
+
+const subscribeBtn = document.querySelector(".a1-newsletter button");
+
+const messageElement = document.querySelector(".a1-newsletter div");
+
+subscribeBtn.addEventListener("click", async function () {
+  const email = emailInput.value;
+  if (email === "") {
+    messageElement.textContent = "Please enter your email";
+    setTimeout(() => {
+      messageElement.textContent = "";
+    }, 1500);
+  } else {
+    const response = await postData(
+      "https://api.formswala.com/api/v1/newsletter/new/subscriber",
+      {
+        email,
+      }
+    );
+    messageElement.textContent = response.message;
+    setTimeout(() => {
+      messageElement.textContent = "";
+    }, 1500);
+  }
+});
+
+async function postData(url, data) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
